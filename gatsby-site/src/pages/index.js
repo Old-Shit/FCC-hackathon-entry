df31978cf276a7abd50b087b34a6971681b5b766
Frank Faustino <nawzyah@gmail.com>
2018-10-17 15:30:08 -0700

    Initial commit

---
+++ gatsby-site/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Heyo 👋</h1>
+    <p>My name's Frank Faustino</p>
+    <p>I'm excited to be a part of the FreeCodeCamp Hackathon!</p>
+  </Layout>
+)
+
+export default IndexPage
